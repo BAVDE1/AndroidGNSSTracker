0b5a6e891d89a4e6a58863ec4ebc3d8e56718732
BAVDE1 <bavde4@gmail.com>
2025-03-16 11:23:19 +1100

    wonky alignments stuff

--- app/src/main/java/com/example/androidtest001/Logger.kt
+++ app/src/main/java/com/example/androidtest001/Logger.kt
@@ -1,14 +1,13 @@
 package com.example.androidtest001
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -33,16 +32,48 @@ class LoggerSingleton {
 
   @Composable
   fun LoggingUnit(pv: PaddingValues) {
+    val scrollState: ScrollState = rememberScrollState()
+
+    val closeBtn: @Composable () -> Unit = {
+      InteractionRelease(
+      onRelease = { println("pressed") },
+      inner = { Box(Modifier.size(25.dp).clip(RoundedCornerShape(5.dp)).background(Color.DarkGray)) { Text(text = "X", modifier = Modifier.align(Alignment.Center))} })
+    }
+
     Box(Modifier.fillMaxSize().padding(pv)) {
       Box(
-        modifier = Modifier.align(Alignment.BottomStart).background(Color.White).fillMaxWidth().height(height)
-          .padding(10.dp)
+        modifier = Modifier.align(Alignment.BottomStart).background(Color.White)
+          .fillMaxWidth().height(height).padding(10.dp)
       ) {
         Column {
-          InteractionRelease(
-            onRelease = { println("pressed") },
-            inner = { Text(text = "button") })
-          Text(text = "help", color = Color.Black)
+          Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(text = "[some toggles]", color = Color.Black)
+            closeBtn()
+          }
+          Row(Modifier.verticalScroll(scrollState).horizontalScroll(scrollState)
+            .fillMaxSize().background(Color.Red).weight(1f, true), verticalAlignment = Alignment.Bottom) {
+            Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
+              Text(text = "1")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+              Text(text = "2")
+            }
+          }
         }
       }
     }
